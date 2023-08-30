0e85a0a699e0768b41d34a4e93f1bb07701c8b18
dieudonneopilam <dieudonneopilam2@gmail.com>
2023-08-31 01:02:51 +0200

    creating architecture of application

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
