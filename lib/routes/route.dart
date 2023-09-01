14037a4a07d55d4515da13d0391950f07df0085d
dieudonneopilam <dieudonneopilam2@gmail.com>
2023-09-01 10:31:52 +0200

    named apk

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:theme/logic/bloc/bottom_navigation_bar_bloc.dart';
+import 'package:theme/logic/bottom/bottom_navigation_bar_bloc.dart';
 import 'package:theme/screens/bottom.dart';
 
 class RouteApp {
--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:heroicons/heroicons.dart';
-import 'package:theme/logic/bloc/bottom_navigation_bar_bloc.dart';
+import 'package:theme/logic/bottom/bottom_navigation_bar_bloc.dart';
 import 'package:theme/screens/home.dart';
 
 class BottomNavigationBarPage extends StatelessWidget {
